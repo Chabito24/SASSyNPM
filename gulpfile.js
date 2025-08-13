61cadaae38f8844f82d3122e335db2886c744695
Edward Martinez <emartinezj1802@gmail.com>
2025-08-13 11:15:38 -0500

    se crean archivos parciales de CSS inician con guuion bajo ejemplo :variables y se incluye en app.scss la linea con use para importar las lineas de codigo a app.scss y compilarlo en conjunto

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,16 @@
-import { src, dest, watch } from 'gulp'
-import * as dartSass from 'sass'
+import { src, dest, watch } from 'gulp';
+import * as dartSass from 'sass';
 
-import gulpSass from 'gulp-sass' //dependencia para usar sass  en el archivo de gulpfile
+import gulpSass from 'gulp-sass'; //dependencia para usar sass  en el archivo de gulpfile
 
 
-const sass = gulpSass(dartSass)
+const sass = gulpSass(dartSass);
 
-export function css(done){
+export function css(done) {
     src('src/scss/app.scss') //ubica el archivo
-        .pipe(sass()) //aplica sass
+        .pipe(sass().on('error', sass.logError)) //aplica sass, // .on....<--- maneja errores de compilación
         .pipe(dest('build/css')) //.pipe controla el orden en el cual se va a ir ejecutando las funciones
-    done()
+    done();
 }
 
 //todo el codigo anterior hace lo mismo que enrutar el archivo de css , aqui borre la carpeta build y volvi a correr nmp run css y crea nuevamente la carpeta de build/css y sus archivo app.css
@@ -23,7 +23,7 @@ export function css(done){
     done()
 }*/
 
-export function dev () {
-    watch('src/scss/app.scss', css)
+export function dev() {
+    watch('src/scss/app.scss', css);
 
 } //watch con gulp, en el primer import agregamos como parametro watch se usa en la ocnsola npm run dev para ejecutar y Ctrl+c para finalizar
